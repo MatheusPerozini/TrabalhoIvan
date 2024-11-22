953a33e139dad62b8ddb2838f3d349bc1eb527b2
Matheus Perozini Magalhães <perozinimagalhesmatheus@gmail.com>
2024-11-22 14:06:20 -0300

    Flip image

--- main.py
+++ main.py
@@ -5,13 +5,24 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 detector = HandDetector(detectionCon=0.8)
+colorR = (255,0,255)
 
 while True:
     sucess, img = cap.read()
+    # Se for necessario inverter a imagem
+    img = cv2.flip(img, 1)
+
     img = detector.findHands(img)
     lmList, _ = detector.findPosition(img)
 
-    cv2.rectangle(img, (100, 100), (300, 300), (255,0,255), cv2.FILLED)
+    if lmList:
+        cursor = lmList[8]
+        if 100 < cursor[0] < 300 and 100 < cursor[1]:
+            colorR = (0, 255, 0)
+        else:
+            colorR = (255, 0, 255)
+
+    cv2.rectangle(img, (100, 100), (300, 300), colorR, cv2.FILLED)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
